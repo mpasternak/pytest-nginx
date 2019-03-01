a78b20a647b47b8f5cf56a1e166dd660a90e5468
Michał Pasternak <michal.dtz@gmail.com>
2019-03-01 13:32:40 +0100

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup_requires = [
 
 setup(
     name='pytest-nginx-iplweb',
-    version='1.1.0',
+    version='1.1.1',
     description='nginx fixture for pytest - iplweb temporary fork',
     long_description=(open('README.rst').read()),
     keywords='tests py.test pytest fixture nginx',
